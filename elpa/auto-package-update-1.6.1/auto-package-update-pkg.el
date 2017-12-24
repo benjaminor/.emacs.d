45b20b745e3b57de0bc54088e12995dd6ba0f0eb
benjaminor <benni.orthen@gmail.com>
2017-12-24 00:01:36 -0500

    removed ignored files (s.a. elpa, session files)

--- elpa/auto-package-update-1.6.1/auto-package-update-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "auto-package-update" "1.6.1" "Automatically update Emacs packages." '((emacs "24.4") (dash "2.1.0")) :commit "cdef79f9fc6f8347fdd05664978fb9a948ea0410" :url "http://github.com/rranelli/auto-package-update.el" :keywords '("package" "update"))
