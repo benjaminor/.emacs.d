d4eadfab10aa7ebf0e1687b0172afec7d744a5ce
ben <benjamin.orthen@bmw.de>
2018-07-09 14:09:33 +0200

    Added better window splitting.

--- elisp/base-functions.el
+++ elisp/base-functions.el
@@ -33,6 +33,26 @@
   (let ((default-directory "~/Documents/Python/"))
     (call-interactively 'helm-find-files)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Custom splitting functions ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun vsplit-last-buffer ()
+  (interactive)
+  (split-window-vertically)
+  (other-window 1 nil)
+  (switch-to-next-buffer)
+  )
+(defun hsplit-last-buffer ()
+  (interactive)
+  (split-window-horizontally)
+  (other-window 1 nil)
+  (switch-to-next-buffer)
+  )
+
+(global-set-key (kbd "C-x 2") 'vsplit-last-buffer)
+(global-set-key (kbd "C-x 3") 'hsplit-last-buffer)
+
 ;; Reload buffer with F5
 (global-set-key (kbd "<f5>") 'revert-buffer)
 
