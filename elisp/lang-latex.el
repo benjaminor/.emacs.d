329bf04586cdf0953215d96a4e2fbfede866ad0e
Benjamin Orthen <benjamin@orthen.net>
2020-02-14 17:15:07 +0100

    Rewrite lang-latex with macros from use-package

--- elisp/lang-latex.el
+++ elisp/lang-latex.el
@@ -9,16 +9,20 @@
 (use-package tex
   :quelpa (auctex)
   :mode ("\\.tex\\'" . TeX-latex-mode)
+  :hook
+  ((LaTeX-mode . visual-line-mode)
+   (LaTeX-mode . flyspell-mode)
+   (LaTeX-mode . LaTeX-math-mode)
+   (LaTeX-mode . turn-on-reftex))
   :config
-  (setq TeX-auto-save t)
-  (setq TeX-parse-self t)
+  (setq TeX-auto-save t
+		TeX-parse-self t
+		TeX-save-query nil
+		TeX-PDF-mode t)
+  (setq bibtex-dialect 'biblatex)
   (setq-default TeX-master nil)
-  (add-hook 'LaTeX-mode-hook 'visual-line-mode)
-  (add-hook 'LaTeX-mode-hook 'flyspell-mode)
-  (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
   (setq reftex-plug-into-AUCTeX t)
-  (setq TeX-PDF-mode t)
+  (TeX-global-PDF-mode t)
   (setq reftex-bibliography-commands '("bibliography" "nobibliography" "addbibresource"))
 
 
@@ -26,22 +30,27 @@
 
   ;; " expands into csquotes macros
   (setq LaTeX-csquotes-close-quote "}"
-	LaTeX-csquotes-open-quote "\\enquote{")
+		LaTeX-csquotes-open-quote "\\enquote{")
   ;; company and yasnippet setup is handled in text-completion
   (add-hook 'TeX-mode-hook 'prettify-symbols-mode)
   ;; Don't use Helm for the reftex-citation lookup
   (eval-after-load 'helm-mode
-    '(add-to-list 'helm-completing-read-handlers-alist '(reftex-citation . nil))))
+	'(add-to-list 'helm-completing-read-handlers-alist '(reftex-citation . nil))))
 
 (use-package latex-preview-pane
   :config
   (latex-preview-pane-enable))
 
+(use-package auctex-latexmk
+  :config
+  (setq auctex-latexmk-inherit-TeX-PDF-mode t)
+  (auctex-latexmk-setup))
 
 (use-package reftex
   :ensure nil
   :after auctex
   :hook (LaTeX-mode . reftex-mode))
 
+
 (provide 'lang-latex)
-;;; latex.el ends here
+;;; lang-latex.el ends here
