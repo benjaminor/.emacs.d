987a7b2761d5480bd5141959f72b9486e6f7f748
Benjamin Orthen <benjamin@orthen.net>
2020-02-14 16:43:56 +0100

    Remove semantic, ede, ggtags, add font-lock for cmake, cpp
    * Also add disaster, helm-ctest

--- elisp/lang-c.el
+++ elisp/lang-c.el
@@ -5,6 +5,8 @@
 (use-package cc-mode
   :ensure nil
   :config
+  (add-to-list 'auto-mode-alist '("\\.ipp\\'" . c++-mode))
+
   (define-key c-mode-map  [(tab)] 'company-complete)
   (define-key c++-mode-map  [(tab)] 'company-complete)
   (setq-default c-basic-offset 4
@@ -14,51 +16,31 @@
 				indent-tabs-mode t)
   (define-key c-mode-base-map (kbd "RET") 'newline-and-indent))
 
-(use-package semantic
-  :disabled
-  :config
-  (global-semanticdb-minor-mode 1)
-  (global-semantic-idle-scheduler-mode 1)
-  (global-semantic-stickyfunc-mode 1)
-  (semantic-mode 1))
-
-(use-package ede
-  :config
-  ;; enable EDE only in C/C++
-  (global-ede-mode))
-
-;; tags for code navigation
-(use-package ggtags
-  :config
-  (ggtags-mode 1)
-  (add-hook 'c-mode-common-hook
-			(lambda ()
-			  (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-				(ggtags-mode 1))))
-  (dolist (map (list ggtags-mode-map))
-    (define-key map (kbd "C-c g s") 'ggtags-find-other-symbol)
-    (define-key map (kbd "C-c g h") 'ggtags-view-tag-history)
-    (define-key map (kbd "C-c g r") 'ggtags-find-reference)
-    (define-key map (kbd "C-c g f") 'ggtags-find-file)
-    (define-key map (kbd "C-c g c") 'ggtags-create-tags)
-    (define-key map (kbd "C-c g u") 'ggtags-update-tags)
-    (define-key map (kbd "M-.")     'ggtags-find-tag-dwim)
-    (define-key map (kbd "M-,")     'pop-tag-mark)
-    (define-key map (kbd "C-c <")   'ggtags-prev-mark)
-    (define-key map (kbd "C-c >")   'ggtags-next-mark)))
-
-
 (use-package cmake-mode
   :defer t
-  :init				; Add cmake listfile names to the mode list.
+  :init             ; Add cmake listfile names to the mode list.
   (setq auto-mode-alist
 		(append
 		 '(("CMakeLists\\.txt\\'" . cmake-mode))
 		 '(("\\.cmake\\'" . cmake-mode))
 		 auto-mode-alist)))
 
+(use-package cmake-font-lock
+  :defer t
+  :after cmake-mode
+  :hook
+  (cmake-mode cmake-font-lock-activate)
+  :config
+  (autoload 'cmake-font-lock-activate "cmake-font-lock" nil t))
 
+(use-package modern-cpp-font-lock
+  :config
+  (modern-c++-font-lock-global-mode))
 
+(use-package disaster)
+
+(use-package helm-ctest
+  :after helm)
 
 (provide 'lang-c)
-;;; lang-cc.el ends here
+;;; lang-c.el ends here
