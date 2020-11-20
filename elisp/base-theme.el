680f72cbbad8eea33015a0af85f84611c2eb90bd
Benjamin Orthen <benjamin@orthen.net>
2020-11-20 16:56:09 +0100

    ignore eln-cache dir, change some settings for solarized theme

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -11,10 +11,10 @@
   (setq solarized-distinct-fringe-background t)
 
   ;; Don't change the font for some headings and titles
-  (setq solarized-use-variable-pitch nil)
+  ;; (setq solarized-use-variable-pitch nil)
 
   ;; make the modeline high contrast
-  (setq solarized-high-contrast-mode-line t)
+  ;; (setq solarized-high-contrast-mode-line t)
 
   ;; Use less bolding
   (setq solarized-use-less-bold t)
@@ -23,7 +23,7 @@
   (setq solarized-use-more-italic t)
 
   ;; Use less colors for indicators such as git:gutter, flycheck and similar
-  (setq solarized-emphasize-indicators nil)
+  ;; (setq solarized-emphasize-indicators nil)
 
   ;; Don't change size of org-mode headlines (but keep other size-changes)
   (setq solarized-scale-org-headlines nil)
