3758da05925df32b060f195821ee3648d9ea4e86
Benjamin Orthen <benjamin@orthen.net>
2020-12-18 16:29:07 +0100

    remove rust-mode, use only rustic for rust development

--- elisp/lang-rust.el
+++ elisp/lang-rust.el
@@ -3,34 +3,14 @@
 ;;; Commentary:
 ;; Taken from http://emacs-bootstrap.com/
 
-;; rust-mode
-;; https://github.com/rust-lang/rust-mode
-
 ;;; Code:
 
-(use-package rust-mode
-  :bind ( :map rust-mode-map
-		  (("C-c C-t" . racer-describe)))
-  :config
-  ;; add flycheck support for rust
-  ;; https://github.com/flycheck/flycheck-rust
-  (use-package flycheck-rust
-	:config
-	(with-eval-after-load 'rust-mode
-	  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
-
-  ;; cargo-mode for all the cargo related operations
-  (use-package cargo)
-
-  (add-hook 'rust-mode-hook 'flycheck-mode)
-  (add-hook 'flycheck-mode-hook 'flycheck-rust-setup)
-  (add-hook 'rust-mode-hook 'racer-mode)
-  (add-hook 'rust-mode-hook 'cargo-minor-mode))
-
 (use-package rustic
-  :after rust-mode
+  :after lsp
   :config
-  (setq rustic-lsp-server 'rust-analyzer))
+  (setq lsp-rust-analyzer-cargo-watch-command "clippy")
+  (setq rustic-lsp-server 'rust-analyzer)
+  (rustic-doc-mode))
 
 (provide 'lang-rust)
 ;;; lang-rust.el ends here
