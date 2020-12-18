f88bde9cec55b1aab1f4acbd05a97cc59ffc4604
Benjamin Orthen <benjamin@orthen.net>
2020-12-18 17:05:47 +0100

    use org-ql for custom agenda queries

--- elisp/org-setup.el
+++ elisp/org-setup.el
@@ -179,48 +179,75 @@
 (use-package org-super-agenda
   :after org
   :config
-  (org-super-agenda-mode 1)
+  (org-super-agenda-mode t)
   (setq org-super-agenda-groups
 		'(;; Each group has an implicit boolean OR operator between its selectors.
-		  (:name "Today"  ; Optionally specify section name
-				 :time-grid t  ; Items that appear on the time grid
-				 :todo "TODAY")  ; Items that have this TODO keyword
-		  (:name "Important"
-				 ;; Single arguments given alone
-				 :tag "bills"
-				 :priority "A")
-		  ;; Set order of multiple groups at once
-		  (:order-multi (2 (:name "Shopping in town"
-								  ;; Boolean AND group matches items that match all subgroups
-								  :and (:tag "shopping" :tag "@town"))
-						   (:name "Food-related"
-								  ;; Multiple args given in list with implicit OR
-								  :tag ("food" "dinner"))
-						   (:name "Personal"
-								  :habit t
-								  :tag "personal")
-						   (:name "Space-related (non-moon-or-planet-related)"
-								  ;; Regexps match case-insensitively on the entire entry
-								  :and (:regexp ("space" "NASA")
-												;; Boolean NOT also has implicit OR between selectors
-												:not (:regexp "moon" :tag "planet")))))
-		  ;; Groups supply their own section names when none are given
-		  (:todo "WAITING" :order 8)  ; Set order of this section
-		  (:todo ("SOMEDAY" "TO-READ" "CHECK" "TO-WATCH" "WATCHING")
-				 ;; Show this group at the end of the agenda (since it has the
-				 ;; highest number). If you specified this group last, items
-				 ;; with these todo keywords that e.g. have priority A would be
-				 ;; displayed in that group instead, because items are grouped
-				 ;; out in the order the groups are listed.
-				 :order 9)
-		  (:priority<= "C"
-					   ;; Show this section after "Today" and "Important", because
-					   ;; their order is unspecified, defaulting to 0. Sections
-					   ;; are displayed lowest-number-first.
-					   :order 1)
-		  ;; After the last group, the agenda will display items that didn't
-		  ;; match any of these groups, with the default order position of 99
-		  )))
+		  (:name "Next tasks"  ; Optionally specify section name
+				 :and (:scheduled t
+								  :todo ("TODO")))
+		  (:name "Projects"
+				 :and (:children t
+								 :todo ("TODO" "NEXT"))))))
+
+(use-package org-ql
+  :after org
+  :config
+  (defun my/custom-org-agenda ()
+    "Show common tasks that are scheduled now"
+
+    (interactive)
+    (org-ql-search (org-agenda-files)
+      '(and (or (and (or (ts-active :on today)
+						 (deadline auto)
+						 (scheduled :to today))
+					 (or
+					  (ancestors "Common Tasks")
+					  (tags "buy")))
+				(and (ancestors "Schulden")
+					 (or
+					  (parent "Haben")
+					  (parent "Soll"))))
+			(not (todo "DONE" "WAITING")))
+      :title "My Agenda View"
+      ;; The `org-super-agenda-groups' setting is used automatically when set, or it
+      ;; may be overriden by specifying it here:
+      :super-groups '((:name "Schulden: noch begleichen"
+							 :tag "soll"
+							 :order 4)
+					  (:name "Schulden: noch bekommen"
+							 :tag "haben"
+							 :order 5)
+					  (:name "Tasks"
+							 :todo "TODO"
+							 :order 1)
+					  (:name "Buy stuff"
+							 :tag "buy"
+							 :order 7)
+					  (:todo "WAITING"
+							 :order 9)
+					  (:discard (:anything t)))))
+
+  (define-key global-map (kbd "C-c u") 'my/custom-org-agenda)
+
+
+  (defun my/custom-projects-agenda ()
+    "Show tasks of projects."
+    (interactive)
+    (org-ql-search (org-agenda-files)
+	  '(and (not (outline-path "Common Tasks"))
+			(not (outline-path "Finanzen" "Schulden"))
+			(or (todo "NEXT")
+				(and (not (todo "DONE" "WAITING"))
+					 (or (ts-active :on today)
+						 (deadline auto)
+						 (scheduled :to today)))))
+	  :title "My projects"
+	  :super-groups '((:auto-outline-path t))))
+
+
+  (define-key global-map (kbd "C-c u") 'my/custom-org-agenda)
+  (define-key global-map (kbd "C-c i") 'my/custom-projects-agenda))
+
 
 
 (provide 'org-setup)
