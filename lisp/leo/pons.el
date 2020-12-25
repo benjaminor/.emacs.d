6e8aa3e16256be76456ef80a88c5660a71795043
Benjamin Orthen <benjamin@orthen.net>
2020-12-25 16:18:04 +0100

    ignore some files, add pons.el (as a start)

---
+++ lisp/leo/pons.el
@@ -0,0 +1,10 @@
+;;; pons.el -- scrape the PONS dictionary to translate words
+
+;;; Commentary:
+
+;; request URL: https://de.pons.com/übersetzung/<translate-service>/<word>/
+
+;;; Code:
+
+(provide 'pons)
+;;; pons.el ends here
